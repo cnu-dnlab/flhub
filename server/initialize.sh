ad51aea6a2e10b79ccf73809582170805422cdf8
munhyunsu <munhyunsu@gmail.com>
2022-06-12 08:27:50 +0900

    Caution message for initialize

--- server/initialize.sh
+++ server/initialize.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-python3 -m grpc_tools.protoc -I./ --python_out . --grpc_python_out . flhub.proto
-python3 main_database_init.py --reset
+if [ "$1" != "yes" ] && [ "$1" != "YES" ];
+then
+    echo "CAUTION SCRIPT"
+    echo "Execute $0 yes"
+    exit 0
+fi
+
+#python3 -m grpc_tools.protoc -I./ --python_out . --grpc_python_out . flhub.proto
+#python3 main_database_init.py --reset

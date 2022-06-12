23bc07b90232d633af0ff1531c0155c2af69ea23
munhyunsu <munhyunsu@gmail.com>
2022-06-12 19:40:27 +0900

    uncomment initialize script

--- server/initialize.sh
+++ server/initialize.sh
@@ -7,5 +7,5 @@ then
     exit 0
 fi
 
-#python3 -m grpc_tools.protoc -I./ --python_out . --grpc_python_out . flhub.proto
-#python3 main_database_init.py --reset
+python3 -m grpc_tools.protoc -I./ --python_out . --grpc_python_out . flhub.proto
+python3 main_database_init.py --reset

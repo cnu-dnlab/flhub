57ca00e707077f5358b197b6734ac5a1feca4fb3
munhyunsu <munhyunsu@gmail.com>
2022-06-13 18:15:28 +0900

    Database connection with cred

--- server/main_database_init.py
+++ server/main_database_init.py
@@ -14,6 +14,22 @@ def main():
         print(f'Parsed arguments {FLAGS}')
         print(f'Unparsed arguments {_}')
 
+    try:
+        conn = mariadb.connect(
+                 user=secret.dbuser,
+                 password=secret.dbpassword,
+                 host=secret.dbhost,
+                 port=secret.dbport,
+                 database=secret.dbname)
+    except mariadb.Error as e:
+        print(f'[{int(time.time()-STIME)}] Error connecting to MariaDB: {e}')
+        sys.exit(0)
+    cur = conn.cursor()
+
+
+    conn.commit()
+    conn.close()
+
 
 if __name__ == '__main__':
     root_path = os.path.abspath(__file__)

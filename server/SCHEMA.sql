047e5f11cff9aed5490af2665e7f099b7b28ea8a
munhyunsu <munhyunsu@gmail.com>
2022-08-22 10:53:48 +0900

    schema

--- server/SCHEMA.sql
+++ server/SCHEMA.sql
@@ -1,8 +1,9 @@
 -- Why we use file storage not RDBMS file arhive?
 -- https://www.brentozar.com/archive/2021/07/store-files-in-a-file-system-not-a-relational-database/
 
+-- For authentication
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT,
+  id INT UNSIGNED AUTO_INCREMENT,
   username TEXT NOT NULL,
   salt TEXT NOT NULL,
   hpassword TEXT NOT NULL,
@@ -10,32 +11,44 @@ CREATE TABLE IF NOT EXISTS users (
   UNIQUE (username)
 );
 
+-- userid: For model uploader
 CREATE TABLE IF NOT EXISTS models (
-  id INT AUTO_INCREMENT,
-  userid INT NOT NULL,
+  id INT UNSIGNED AUTO_INCREMENT,
+  userid INT UNSIGNED NOT NULL,
   modelname TEXT NOT NULL,
   detail TEXT NOT NULL,
   PRIMARY KEY (id),
+  FOREIGN KEY (userid) REFERENCES users (id),
   UNIQUE (userid, modelname)
 );
 
+-- modelid: Base model
+-- parentid: parent version id
 CREATE TABLE IF NOT EXISTS versions (
-  id INT AUTO_INCREMENT,
-  modelid INT NOT NULL,
-  major INT NOT NULL,
-  minor INT NOT NULL,
-  micro INT NOT NULL,
+  id INT UNSIGNED AUTO_INCREMENT,
+  modelid INT UNSIGNED NOT NULL,
+  parentid INT UNSIGNED DEFAULT 0,
+  major INT UNSIGNED NOT NULL,
+  minor INT UNSIGNED NOT NULL,
+  micro INT UNSIGNED NOT NULL,
   location TEXT NOT NULL,
-  PRIMARY KEY(id),
+  PRIMARY KEY (id),
+  FOREIGN KEY (modelid) REFERENCES models (id),
   UNIQUE (modelid, major, minor, micro)
 );
 
+-- status: 0: normal, 1: merge waiting, 2: merged
 CREATE TABLE IF NOT EXISTS commits (
-  id INT AUTO_INCREMENT,
-  versionid INT NOT NULL,
-  userid INT NOT NULL,
+  id INT UNSIGNED AUTO_INCREMENT,
+  userid INT UNSIGNED NOT NULL,
+  versionidfrom INT UNSIGNED NOT NULL,
+  versionidto INT UNSIGNED DEFAULT 0,
+  revision INT UNSIGNED NOT NULL,
   location TEXT NOT NULL,
-  status TINYINT(2) UNSIGNED DEFAULT 0, -- 0: normal, 1: merge waiting, 2: merged
+  status TINYINT(2) UNSIGNED DEFAULT 0,
   PRIMARY KEY (id),
-  UNIQUE (versionid, userid) -- One commit per one user
+  FOREIGN KEY (userid) REFERENCES users (id),
+  FOREIGN KEY (versionid) REFERENCES versions (id),
+  UNIQUE (versionid, userid, revision)
 );
+
